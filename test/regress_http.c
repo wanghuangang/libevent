d83298c1b8de9e718ef14c9ba7281e4d5aca3cb1
Azat Khuzhin <a3at.mail@gmail.com>
2015-11-05 14:19:14 +0300

    test/regress_http: with defer retry goes to the second stage

--- test/regress_http.c
+++ test/regress_http.c
@@ -3465,7 +3465,8 @@ http_connection_retry_test_basic(void *arg, const char *addr, struct evdns_base 
 	} else {
 		SSL *ssl = SSL_new(get_ssl_ctx());
 		bev = bufferevent_openssl_socket_new(
-			data->base, -1, ssl, BUFFEREVENT_SSL_CONNECTING, 0);
+			data->base, -1, ssl, BUFFEREVENT_SSL_CONNECTING,
+			BEV_OPT_DEFER_CALLBACKS);
 		// bufferevent_openssl_set_allow_dirty_shutdown(bev, 1);
 	}
 	evcon = evhttp_connection_base_bufferevent_new(data->base, dns_base, bev, addr, hs.port);
